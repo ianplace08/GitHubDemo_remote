e0abb5d37ba2e214f1df3ca0fcb3bf74becdc267
Ian <ian.placencia@pucp.edu.pe>
2022-02-17 17:50:31 -0500

    Create SecondView

---
+++ GitHubDemo/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GitHubDemo
+//
+//  Created by Ian Placencia on 17/02/22.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
