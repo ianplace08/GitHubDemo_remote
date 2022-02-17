2c0b313a3c364c8ad770571a3226a0a4fc6a04ad
Ian <ian.placencia@pucp.edu.pe>
2022-02-17 17:42:39 -0500

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Ian Placencia on 17/02/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
